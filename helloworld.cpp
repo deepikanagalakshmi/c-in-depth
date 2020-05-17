63e570d88d629542996e54e90568f0f4f9ca8878
Deepika Nagalakshmi <58824208+deepikanagalakshmi@users.noreply.github.com>
2020-05-17 19:48:56 +0530

    1st program in cpp

---
+++ helloworld.cpp
@@ -0,0 +1,20 @@
+#include<cstdio>
+using namespace std;
+int main() {
+    puts("Click on button");
+    puts("Create a new player");
+    puts("Add life to the player");
+    return 0;//return an exit with 0 code
+}
+/*
+here i can
+write multi-line comment
+*/
+//void returns nothing
+void nothing() {
+    puts("Hello World");
+}
+//char returns char
+char iamchar() {
+    return 'd';
+}
