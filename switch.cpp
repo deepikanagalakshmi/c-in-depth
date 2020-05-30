c1cf529fd994562d613ff8f963ecd27e89032dda
Deepika Nagalakshmi <58824208+deepikanagalakshmi@users.noreply.github.com>
2020-05-30 10:58:17 +0530

    Create switch.cpp

---
+++ switch.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int ratings;
+	cout << "Enter ratings";
+	cin >> ratings;
+
+	switch (ratings) {
+	case 1:
+		puts("Rated as 1");
+		break;
+	case 2:
+		puts("Rated as 2");
+		break;
+	case 3:
+		puts("Rated as 3");
+		break;
+	case 4:
+		puts("Rated as 4");
+		break;
+	case 5:
+		puts("Rated as 5");
+		break;
+	default:
+		puts("Please enter your ratings between 1 and 5");
+
+	}
+}
