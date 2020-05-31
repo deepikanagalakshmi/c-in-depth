c9317f318fa4599cf1a535e06121a6f6c28beba1
Deepika Nagalakshmi <58824208+deepikanagalakshmi@users.noreply.github.com>
2020-05-31 21:28:22 +0530

    11th program

---
+++ forForrange.cpp
@@ -0,0 +1,20 @@
+using namespace std;
+int main()
+{
+	int nums[] = { 1,2,3,4,5,6 };
+
+	cout << "Using for loop" << endl;
+
+	for (int i = 0; i < 6; i++)
+	{
+		cout << nums[i] << endl;
+	}
+
+	cout << "Using range based for loop" << endl;
+
+	for (int i : nums) {
+		cout << i << endl;
+	}
+	return 0;
+
+}
