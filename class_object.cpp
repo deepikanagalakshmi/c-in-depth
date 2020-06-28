6114027f8474fce7e0fd89f87c3e7b10ddc782b3
Deepika Nagalakshmi <58824208+deepikanagalakshmi@users.noreply.github.com>
2020-06-28 21:46:39 +0530

    Create class_object.cpp
    functions inside the class are called as methods.
    CLASS:  A class in C++ is the building block, that leads to Object-Oriented programming. It is a user-defined data type, which holds its own data members and member functions, which can be accessed and used by creating an instance of that class.
    OBJECT: An Object is an instance of a Class. When a class is defined, no memory is allocated but when it is instantiated (i.e. an object is created) memory is allocated.

---
+++ class_object.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class User
+{
+	string secret = "This is secret message";
+	int secret_no = 12233;
+public:
+	string name = "default";
+	void classMessage() {
+		cout << "Class is great, " << name << endl;
+	}
+};
+
+int main(){
+	//We can create many object for the same class.
+	//Each object can perform their own operations independent of other object.
+
+	User sam;
+	sam.name = "sam";
+	sam.classMessage();
+	//sould not access secret variable because as default state is private 
+	//sam.secret = "hello";
+
+	User mane;
+	mane.name = "Mane";
+	mane.classMessage();
+
+	User deepika;
+	deepika.classMessage();
+	deepika.name = "Deepika";
+	deepika.classMessage();
+
+	return 0;
+}
