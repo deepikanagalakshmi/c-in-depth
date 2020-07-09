51ebca1770bf67bb96d385227306eaf9e5490f98
Deepika Nagalakshmi <58824208+deepikanagalakshmi@users.noreply.github.com>
2020-07-09 21:00:23 +0530

    PROGRAM 46

---
+++ shared_pointers.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include<memory>
+using namespace std;
+
+class User {
+public:
+	User() {
+		puts("User constructed");
+	}
+	~User() {
+		puts("------------User destroyed-----------");
+	}
+	void testfunc() {
+		puts("Iam a test function");
+	}
+};
+int main()
+{
+
+	{
+		//unique_ptr<User> sam (new User()); //Not a fav. one for developers because of using new keyword
+		unique_ptr<User> sam = make_unique<User>();
+		sam->testfunc();
+
+		//unique_ptr<User> sam = samm; //NOT ALLOWED
+	}
+
+	{
+		//shared_ptr<User> tim (new User()); //Not good way
+		shared_ptr<User> tim = make_shared<User>();
+		//Copying is allowed
+		shared_ptr<User> timm = tim;
+
+		tim->testfunc();
+		timm->testfunc();
+	}
+
+	cout << "Outside the scope\n";
+}
+
