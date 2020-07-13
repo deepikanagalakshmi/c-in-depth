27cf23306cacd88c7bafec8065fc48394023d073
Deepika Nagalakshmi <58824208+deepikanagalakshmi@users.noreply.github.com>
2020-07-13 19:28:07 +0530

    PROGRAM 55
    0.1
    1.2
    3.1

--- STL_functors.cpp
+++ STL_functors.cpp
@@ -21,8 +21,8 @@ int main()
 	floaty.getValue();
   
 	//below code cannot be achieved by constructor by passing values becasue constructors runs for very first time only.
-         //so we make it work by using functors
-	floaty(1.1); //same as floaty.addvalue(1.1)
+        //so we make it work by using functors
+	floaty(1.1); //same as floaty.addvalue(1.1) 
 	floaty.getValue();
 
 	floaty(1.9);//same as floaty.addvalue(1.1)
